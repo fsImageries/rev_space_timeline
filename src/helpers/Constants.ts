3aeac6c0c4f2906ba2b085795e8eb8dccd223243
Farooq Singh <farooq.singh@softwareone.com>
2023-06-02 23:55:30 +0200

    Added TIME_SCALE function which controls orb- and axisrotation at once.

--- src/helpers/Constants.ts
+++ src/helpers/Constants.ts
@@ -1,8 +1,16 @@
 const Constants = {
     DISTANCE_SCALE : 3000000,
     SIZE_SCALE: 10,
-    ROT_SCALE: 5000,
-    ORB_SCALE: 1000000,
+    ROT_SCALE: 1,
+    ORB_SCALE: 1,
+
+    _time_scale: 1,
+    get TIME_SCALE() {return this._time_scale},
+    set TIME_SCALE(v:number) {
+        this.ROT_SCALE = v
+        this.ORB_SCALE = v
+        this._time_scale = v
+    }
 }
 
 export default Constants

5cf02b3095e3ca9b4bb962c5ac59568492d1e6e7
Farooq Singh <farooq.singh@softwareone.com>
2023-06-05 09:56:06 +0200

    Did some cleanup.

--- src/app.ts
+++ src/app.ts
@@ -5,23 +5,6 @@ import systemFactory from "./Factories/SystemFactory";
 import celestialData from "./object_data.yaml"
 
 
-// const data = celestialData as SystemJsonData;
-
-// const sys = await systemFactory(data.systems[0])
-// const world = new World(sys)
-// sys.initWorld(world)
-
-// // const obj = world.scene.getObjectByName("tangerineDream_masterGrp")
-// const obj = world.scene.getObjectByName("yellowstone_masterGrp")
-// // const obj:any = null
-// const target = sys.getById(obj.userData["id"])
-// // console.log(target)
-// world.cam.setFollowTarget(target)
-// world.cam.activateThird()
-
-// requestAnimationFrame((n) => World.eventLoop(n, world))
-
-
 new Promise((resolve) => {resolve(null)}).then(async () => {
     const data = celestialData as SystemJsonData;
 

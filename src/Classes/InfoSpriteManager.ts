4316a8b2dd7c2e3d958c2421b3d11f305929f274
Farooq Singh <farooq.singh@softwareone.com>
2023-06-10 18:02:44 +0200

    Done.

--- src/Classes/InfoSpriteManager.ts
+++ src/Classes/InfoSpriteManager.ts
@@ -17,11 +17,12 @@ export class InfoSpriteManager {
     public init() {
         // Needs to run after the init function of its parents
         this._sprites.forEach(([sprite, obj]) => {
-            const side = obj.object.orbit.scale.x > 0 ? -1 : 1;
+            const side = obj.object.orbit ? 
+                obj.object.orbit.scale.x > 0 ? -1 : 1 :
+                1
             const scale = obj.drawRadius / 6.5;
             sprite.scale.setScalar(scale);
             sprite.position.copy(obj.object.masterGrp.position).x += (obj.drawRadius + scale) * side;
-
         })
 
     }

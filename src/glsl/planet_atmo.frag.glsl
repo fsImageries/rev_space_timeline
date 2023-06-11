f53f356473f6d809af12a1ea410b101aa522c810
fsImageries <fsImageries@users.noreply.github.com>
2023-06-11 14:56:39 +0000

    deploy: 237fb7f939827aee89bd95eedb57a5b6ee8d36c2

--- src/glsl/planet_atmo.frag.glsl
+++
@@ -1,8 +0,0 @@
-uniform vec3 glowColor;
-uniform float intensityMult;
-varying float intensity;
-
-void main() {
-  vec3 glow = (glowColor - .2) * intensity * intensityMult;
-  gl_FragColor = vec4( glow, 1.0 );
-}

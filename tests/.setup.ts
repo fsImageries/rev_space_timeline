af3c6ad8d790ef6f36c6ce9a8a75f8c283977c99
Farooq Singh <farooq.singh@softwareone.com>
2023-06-24 20:17:32 +0200

    Fixed tests, mocked document.

--- tests/.setup.ts
+++ tests/.setup.ts
@@ -2,3 +2,6 @@ import { vi } from 'vitest';
 
 const cryptoMock = { randomUUID: vi.fn(() => Math.random().toString()) };
 vi.stubGlobal("crypto", cryptoMock);
+
+const documentMock = { querySelector: vi.fn(() => {}) };
+vi.stubGlobal("document", documentMock);
